32ac1a16d1cd3744cb6cd655199e370489970cbf
edi0328 <edisonthai0328@gmail.com>
2026-04-09 02:06:32 -0400

    Connect pc logic for IF without hazards
    The PC logic requires pc_write from hazard detection to compile.
    This will be implemented once it is done.

--- src/pipeline.vhd
+++ src/pipeline.vhd
@@ -10,3 +10,44 @@ entity pipeline is
     );
 end pipeline;
 
+architecture top of pipeline is
+constant ram_size : integer := 32768;
+signal pc, pc_branch, pc4, next_pc : integer range 0 to ram_size - 1;
+signal pc_sel : std_logic;
+signal i_out : std_logic_vector(31 downto 0);
+
+component memory
+	port(clock, memwrite, memread : in std_logic;
+			writedata: in std_logic_vector(31 downto 0);
+			address : in integer range 0 to ram_size - 1;
+			readdata : out std_logic_vector(31 downto 0);
+			waitrequest : out std_logic);
+end component;
+
+component 
+
+begin
+	I_mem : memory port map (clock => clk, ..., readdata => i_out); -- To be continued
+	D_mem : memory port map (clock => clk, ...);
+	
+	pc4 <= pc + 4;
+	with pc_sel select
+		next_pc <= pc4 when '0',
+						pc_branch when others;
+	
+	
+	process(clk, reset)	
+	begin
+		if reset = '1' then
+			pc <= 0;
+		elsif rising_edge(clk) then
+			if pc_write = '1' and mem_waitrequest = '1' then
+				pc <= next_pc;
+			end if;
+			
+			pc_id <= pc;
+			instruction_id <= i_out;
+			
+		end if;
+	end process;
+end top;
