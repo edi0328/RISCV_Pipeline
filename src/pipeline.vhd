605db89cc13f9c1787784d1457911ef94f54cc11
Clara Mickail <clara.raffla@gmail.com>
2026-04-08 23:05:55 -0400

    Implement ALU, ALU control, registers

--- src/pipeline.vhd
+++ src/pipeline.vhd
@@ -5,8 +5,7 @@ use ieee.numeric_std.all;
 entity pipeline is
 	port (
 		clk : in std_logic;
-		reset : in std_logic;
+		reset : in std_logic
 		-- Feel free to add output ports for debugging below
     );
 end pipeline;
-
